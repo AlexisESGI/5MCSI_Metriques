4382410052c28a3057bd0b5d20caae66c15b3d32
AlexisESGI <93766551+AlexisESGI@users.noreply.github.com>
2024-09-12 15:49:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ def monhistogramme():
 
 @app.route('/commits-data/')
 def commits_data():
-    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    url = 'https://github.com/AlexisESGI/5MCSI_Metriques/commits'
     response = urlopen(url)
     commits_data = json.loads(response.read().decode('utf-8'))
     
